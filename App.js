5d36a72d4e053bc28c20c9fb7f3f838406de8ce5
muhammadhasnain112 <muhammadhasnain12446@gmail.com>
2025-01-03 19:28:04 +0500

    Image slider

---
+++ App.js
@@ -0,0 +1,10 @@
+function abc() {
+    let random = Math.ceil(Math.random() * 7)
+    // console.log(random);
+    
+    let get = document.getElementById("img")
+    get.src = `${random}.avif`
+    
+}
+
+setInterval(abc, 1000)
